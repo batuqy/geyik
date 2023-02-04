e002511738e2d027e10b3928700e74ab7719e134
batuqy <38671054+batuqy@users.noreply.github.com>
2023-02-04 22:45:02 +0300

    al

--- takim_geyik/Assets/Scripts/batu/CardManeger.cs
+++ takim_geyik/Assets/Scripts/batu/CardManeger.cs
@@ -7,16 +7,47 @@ public class CardManeger : MonoBehaviour
     public GameObject[] cardPrefabs;
     
     public int cardNumber=3;
-    
+    List<GameObject> addedCard =new List<GameObject>();
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+
+            DeleteCard();
+
+        }
+    }
+    public void DeleteCard()
+    {
+        if (addedCard.Count > 0)
+        {
+            foreach (GameObject Card in addedCard)
+            {
+                Destroy(Card);
+            }
+        }
+    }
     public void AddCard()
     {
+        
+        if (addedCard.Count>0)
+        {
+            foreach (GameObject Card in addedCard)
+            {
+                Destroy(Card);
+            }
+        }
+        
         int carPositionX = 0;
         for (int i = 0; i < cardNumber; i++)
         {
             
 
             int cardIndex = Random.Range(0, cardPrefabs.Length);
-            Instantiate(cardPrefabs[cardIndex], new Vector2(carPositionX, -3), cardPrefabs[cardIndex].transform.rotation);
+            GameObject Card= Instantiate(cardPrefabs[cardIndex], new Vector2(carPositionX, -3), cardPrefabs[cardIndex].transform.rotation);
+            addedCard.Add(Card);
+
             if (carPositionX < 0)
             {
                 carPositionX = carPositionX * -1;
